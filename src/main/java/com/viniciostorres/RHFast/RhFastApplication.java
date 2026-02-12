1c5014f1cc1aa3e01dd9e3b0fb56334892f1ecaf
ViniciosTorres-Dev <vinicios.santos.torres@gmail.com>
2026-02-12 02:09:22 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/viniciostorres/RHFast/RhFastApplication.java
@@ -0,0 +1,13 @@
+package com.viniciostorres.RHFast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhFastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhFastApplication.class, args);
+	}
+
+}
