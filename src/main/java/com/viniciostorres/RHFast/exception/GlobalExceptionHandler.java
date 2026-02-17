b070c5c11c8bf81f2969100a3983a47fcb8b5de2
ViniciosTorres-Dev <vinicios.santos.torres@gmail.com>
2026-02-17 00:36:24 -0300

    feat: implementar filtro de erro json http

--- src/main/java/com/viniciostorres/RHFast/exception/GlobalExceptionHandler.java
+++ src/main/java/com/viniciostorres/RHFast/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.viniciostorres.RHFast.exception;
 import com.viniciostorres.RHFast.dto.ErroResposta;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +20,20 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .erro("Erro de Regra de Negócio")
                 .mensagem(ex.getMessage())
+                .caminho(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(erroResposta.getStatus()).body(erroResposta);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErroResposta> handleErrosDeLeitura(HttpMessageNotReadableException ex, HttpServletRequest request) {
+
+        ErroResposta erroResposta = ErroResposta.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .erro("Erro de Formatação")
+                .mensagem("Preencha todos os campos obrigatórios, incluindo as listas de seleção.")
                 .caminho(request.getRequestURI())
                 .build();
 
