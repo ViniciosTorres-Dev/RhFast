7df3be49c7eb2c5df8dafd4b229540e75ed2386d
ViniciosTorres-Dev <vinicios.santos.torres@gmail.com>
2026-02-26 23:04:04 -0300

    feat: criação do model currículo para futura implementação de curriculos na aba do candidato.

---
+++ src/main/java/com/viniciostorres/RHFast/recrutamento/model/Curriculo.java
@@ -0,0 +1,15 @@
+package com.viniciostorres.RHFast.recrutamento.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "curriculos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Curriculo {
+}
---
+++ src/main/java/com/viniciostorres/RHFast/recrutamento/service/CurriculoService.java
@@ -0,0 +1,7 @@
+package com.viniciostorres.RHFast.recrutamento.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurriculoService {
+}
